ee23c9c8dab9c7d0cfc84daa9f457ccae3777521
Kaloyanfontys <451641@student.fontys.nl>
2023-06-20 20:13:28 +0200

    COMMIT 2

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState } from 'react'
-import Navbar from './components/Navbar'
+import Navbar from './components/NavBar'
 import LatestWork from "./components/LatestWork";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
